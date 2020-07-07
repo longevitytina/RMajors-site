b65c653f0481c776484a00784c8a685f647fcf00
Tina <longevity.tina@gmail.com>
2020-07-07 16:25:21 -0700

    Adds gatsby image and static image query for component

--- src/components/title.js
+++ src/components/title.js
@@ -1,12 +1,28 @@
-import { Link } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
 import React from "react"
-import Image from "../components/image"
 import titleStyles from "./title.module.css"
+import Img from "gatsby-image"
 
-export default function title() {
+export default () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      file(relativePath: { eq: "filler2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <div className={titleStyles.container}>
-      <Image className={titleStyles.avatar} />
+      <Img
+        className={titleStyles.avatar}
+        fluid={data.file.childImageSharp.fluid}
+        alt="Robert headshot"
+      />
       <div>
         <h1 className={titleStyles.name}>Robert Majors</h1>
         <p className={titleStyles.slogan}>Events Planner and Consultant</p>
