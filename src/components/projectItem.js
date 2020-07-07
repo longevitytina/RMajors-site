e1e417841ce27fad254e1a7ae9a6ddbd6d688023
Tina <longevity.tina@gmail.com>
2020-07-07 16:24:30 -0700

    Initializes project item component

---
+++ src/components/projectItem.js
@@ -0,0 +1,11 @@
+import { Link } from "gatsby"
+import React from "react"
+import Image from "../components/image"
+
+export default function ProjectItem() {
+  return (
+    <div>
+      <h1>projectItem</h1>
+    </div>
+  )
+}
