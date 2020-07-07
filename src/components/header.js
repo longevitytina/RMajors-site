782e2a2355f21a1766d5f8f882eb616000769fd6
Tina <longevity.tina@gmail.com>
2020-07-07 16:23:11 -0700

    Styles

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,8 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `black`,
-      marginBottom: `1.45rem`,
+      background: ``,
+      marginBottom: `0`,
     }}
   >
     <div
