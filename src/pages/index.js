4c458c2a72a7231af923c4ff89f4a1986698dc92
Tina <longevity.tina@gmail.com>
2020-07-07 16:25:33 -0700

    Adds project section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Title from "../components/title"
+import Projects from "../components/projects"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -9,6 +10,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Title />
+    <Projects />
   </Layout>
 )
 
