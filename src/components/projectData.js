bae139c818af1f45479748c713e3c7331f80cdcc
Tina <longevity.tina@gmail.com>
2020-07-08 14:58:42 -0700

    Adds project data

---
+++ src/components/projectData.js
@@ -0,0 +1,23 @@
+export const DEFAULT_PROJECTS = [
+  {
+    id: "1",
+    name: "Project 1",
+    description:
+      "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
+    link: "#",
+  },
+  {
+    id: "2",
+    name: "Project 2",
+    description:
+      "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
+    link: "#",
+  },
+  {
+    id: "3",
+    name: "Project 3",
+    description:
+      "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
+    link: "#",
+  },
+]
