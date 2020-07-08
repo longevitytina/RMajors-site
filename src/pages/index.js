c7acbd3e4909052e74eb6c32535ff517b6ba123f
Tina <longevity.tina@gmail.com>
2020-07-08 14:59:40 -0700

    Cleans up unused inport

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Title from "../components/title"
 import Projects from "../components/projects"
 import Layout from "../components/layout"
