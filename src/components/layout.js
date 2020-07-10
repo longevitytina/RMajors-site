8a20b2571c4202dd65deae87fa0823dad13ef50b
Tina <longevity.tina@gmail.com>
2020-07-10 12:51:47 -0700

    Updates, cleans up

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,19 +6,8 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
   return (
     <>
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div>
         <main>{children}</main>
 
