62f0ff831bb8b002a78e43d852ac640a998ded90
Tina <longevity.tina@gmail.com>
2020-07-14 13:55:48 -0700

    Adds static images to project section (temp)

--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -1,11 +1,10 @@
 import React from "react"
 import itemStyles from "./projectItem.module.css"
-import Img from "gatsby-image"
 
 export default function ProjectItem({ data, name, description, link, image }) {
   return (
     <figure>
-      <Img fluid={image}></Img>
+      <img src={image} alt={name} className={itemStyles.image} />
       <div className={itemStyles.title}>{name}</div>
       <div className={itemStyles.description}>{description}</div>
       <a href={link} className={itemStyles.link}>
