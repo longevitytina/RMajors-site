5bf8842c9b4e2c8fe225140bb659460e42a1a064
Tina <longevity.tina@gmail.com>
2020-07-14 13:54:33 -0700

    Cleans up code

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,22 +6,10 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
   return (
     <>
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div>
         <main>{children}</main>
-
         {/* <footer style={{}}>
           © {new Date().getFullYear()}
           {` `}
