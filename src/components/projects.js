1fbbee5ef3aed4597698605f111451234b22b7bb
Tina <longevity.tina@gmail.com>
2020-07-14 13:56:41 -0700

    Adds header to project section

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,7 +6,7 @@ import projectStyles from "./projects.module.css"
 export default function projects() {
   return (
     <div>
-      {/* <h1 className={projectStyles.header}>Projects</h1> */}
+      <h1 className={projectStyles.heading}>Projects</h1>
       <div className={projectStyles.container}>
         {DEFAULT_PROJECTS.map((project, i) => (
           <ProjectItem {...project} key={i} />
