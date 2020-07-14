ffca44037eeb581c0874209a5db705c272532c24
Tina <longevity.tina@gmail.com>
2020-07-14 13:54:07 -0700

    Adds about section

---
+++ src/components/about.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import aboutStyles from "./about.module.css"
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query AboutQuery {
+      file(relativePath: { eq: "filler.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div className={aboutStyles.container}>
+      <div className={aboutStyles.writingWrap}>
+        <h1 className={aboutStyles.heading}>About</h1>
+        <p className={aboutStyles.summary}>
+          Lomo skateboard glossier butcher flannel actually selfies four loko,
+          disrupt listicle retro. Bitters jianbing mixtape affogato schlitz.
+          Meditation forage kale chips, seitan flannel affogato wayfarers.
+          Butcher meditation la croix pabst chambray drinking vinegar knausgaard
+          intelligentsia. Sriracha franzen asymmetrical hot chicken kinfolk four
+          loko cray, occupy leggings brooklyn.
+        </p>
+      </div>
+      <Img
+        className={aboutStyles.avatar}
+        fluid={data.file.childImageSharp.fluid}
+        alt="Robert headshot"
+      />
+    </div>
+  )
+}
