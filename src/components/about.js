d2dadc828740ba1cc13df0f2c04960e0e61a6800
Tina <longevity.tina@gmail.com>
2020-07-20 12:58:21 -0700

    Adds project data and about me

--- src/components/about.js
+++ src/components/about.js
@@ -20,12 +20,22 @@ export default () => {
       <div className={aboutStyles.writingWrap}>
         <h1 className={aboutStyles.heading}>About</h1>
         <p className={aboutStyles.summary}>
-          Lomo skateboard glossier butcher flannel actually selfies four loko,
-          disrupt listicle retro. Bitters jianbing mixtape affogato schlitz.
-          Meditation forage kale chips, seitan flannel affogato wayfarers.
-          Butcher meditation la croix pabst chambray drinking vinegar knausgaard
-          intelligentsia. Sriracha franzen asymmetrical hot chicken kinfolk four
-          loko cray, occupy leggings brooklyn.
+          My name is Robert Majors, my life has been committed to discovering my
+          passion and learning the best way to develop myself into the person I
+          want to become. This has led me to a lot of humanitarian work and
+          exposure to poorer communities, which includes my existing service to
+          the poor in Las Vegas. The exposure I have had to the human need for
+          sustainable housing is the source of my inspiration for my current
+          endeavors in the study of engineering. I am an established resident of
+          Nevada and a student at the University of Nevada Las Vegas. Skilled in
+          design practices, Microsoft Office, AutoCad, engineering and
+          construction concepts. I fill roles of leadership and teamwork in the
+          community and in organizations including Engineers Without Borders,
+          the Las Vegas Catholic Worker, Tau Beta Pi Honor Society, the Nevada
+          Desert Experience and the American Society of Civil Engineers. There
+          are many projects that I am proud to be a part of and I encourage
+          others to reach out to me if they are interested in working on a
+          project together.
         </p>
       </div>
       <Img
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -5,26 +5,57 @@ import project3 from "../images/management.jpg"
 export const DEFAULT_PROJECTS = [
   {
     id: "1",
-    name: "Organizing",
+    name: "Homeless Housing",
     description:
-      "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
-    link: "#",
+      "A place to live and feel safe is essential to living a quality life. This project in Las Vegas is a start up initiative to provide small manufactured dwellings to those who need them in the community. We plan to have an affordable unit, costing less than $1,000, built every week.",
+    link: "www.foodnotbombslasvegas.com",
     image: project3,
   },
   {
     id: "2",
-    name: "Managment",
+    name: "Serving the Poor",
     description:
-      "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
-    link: "#",
+      "Thousands of people are currently living on the streets of Las Vegas during the hot summers and cold winters. The Las Vegas Catholic Worker has been serving breakfast to this community of people for over 30 years. They are always taking volunteers and provide a great space to create new ideas about how we can serve the poor. ",
+    link: "www.lvcw.org",
     image: project2,
   },
   {
     id: "3",
-    name: "Consulting",
+    name: "Engineers Without Borders: UNLV Student Chapter",
     description:
-      "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
-    link: "#",
+      "Knowledge is a gift to the world and working with Engineers Without Borders allows gifted professionals to share their ability with countries that do not have the resources to meet necessary goals. Working with communities to create infrastructure, EWB engages student volunteers to help organize projects from their local campus. At the University of Nevada Las Vegas Chapter we are currently working on a school house to be built in Guatemala. The current cost for the project is $50,000 and we are always looking for ways to raise funds and volunteers that can help with the project. ",
+    link: "www.ewbunlv.com",
+    image: project1,
+  },
+  {
+    id: "4",
+    name: "MLK Parade nevada desert experience",
+    description:
+      "Every January in Las Vegas is the second Largest Parade in Las Vegas. The Martin Luther King  Jr Parade is a time for the community to take the streets and celebrate a life that was dedicated to equality and justice. I have had the privilege to organize a community float in the parade over the past 5 years and it is always a great place to meet new people and hear a good message. ",
+    link: "http://nevadadesertexperience.org/mlk",
+    image: project1,
+  },
+  {
+    id: "5",
+    name: "Sacred Peace Walk",
+    description:
+      "The Nevada Desert has a lot of history and much of its vast treasure is far from the large cities and bright lights. The Nevada Desert Experience has organized a sacred walk into the desert to honor the land and the people who have lived on it for centuries. During my time coordinating this event I have met various individuals from different backgrounds and faiths on this 60 mile pilgrimage. I recommend doing the Sacred Peace Walk once in your life. ",
+    link: "http://nevadadesertexperience.org/spw",
+    image: project1,
+  },
+  {
+    id: "6",
+    name: "Design",
+    description:
+      "Drawing out or designing plans is how I begin most of my projects. It helps make the idea a little more of a reality and it allows me the opportunity to see into the project with greater depth. ",
+    link: "http://nevadadesertexperience.org/spw",
+    image: project1,
+  },
+  {
+    id: "7",
+    name: "Art",
+    description: "Personal music",
+    link: "https://soundcloud.com/robertmajors",
     image: project1,
   },
 ]
