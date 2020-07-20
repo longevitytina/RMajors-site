5518243613b1613b84b95bc805663abd20848d1e
Tina <longevity.tina@gmail.com>
2020-07-20 15:44:44 -0700

    Adds more project data

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,15 +1,27 @@
-import project1 from "../images/build.jpg"
-import project2 from "../images/planning.jpg"
-import project3 from "../images/management.jpg"
+import project1 from "../images/House1.jpg"
+import project2 from "../images/Feedingthepoor.jpg"
+import project3 from "../images/EWB.jpg"
+import project4 from "../images/parade.jpg"
+import project5 from "../images/peaceWalk.jpg"
+import project6 from "../images/workPortfolio-1.jpg"
+import project7 from "../images/guitar.jpg"
 
 export const DEFAULT_PROJECTS = [
+  {
+    id: "3",
+    name: "Engineers Without Borders: UNLV Student Chapter",
+    description:
+      "Knowledge is a gift to the world and working with Engineers Without Borders allows gifted professionals to share their ability with countries that do not have the resources to meet necessary goals. Working with communities to create infrastructure, EWB engages student volunteers to help organize projects from their local campus. At the University of Nevada Las Vegas Chapter we are currently working on a school house to be built in Guatemala. The current cost for the project is $50,000 and we are always looking for ways to raise funds and volunteers that can help with the project. ",
+    link: "www.ewbunlv.com",
+    image: project3,
+  },
   {
     id: "1",
     name: "Homeless Housing",
     description:
       "A place to live and feel safe is essential to living a quality life. This project in Las Vegas is a start up initiative to provide small manufactured dwellings to those who need them in the community. We plan to have an affordable unit, costing less than $1,000, built every week.",
     link: "www.foodnotbombslasvegas.com",
-    image: project3,
+    image: project1,
   },
   {
     id: "2",
@@ -20,20 +32,12 @@ export const DEFAULT_PROJECTS = [
     image: project2,
   },
   {
-    id: "3",
-    name: "Engineers Without Borders: UNLV Student Chapter",
-    description:
-      "Knowledge is a gift to the world and working with Engineers Without Borders allows gifted professionals to share their ability with countries that do not have the resources to meet necessary goals. Working with communities to create infrastructure, EWB engages student volunteers to help organize projects from their local campus. At the University of Nevada Las Vegas Chapter we are currently working on a school house to be built in Guatemala. The current cost for the project is $50,000 and we are always looking for ways to raise funds and volunteers that can help with the project. ",
-    link: "www.ewbunlv.com",
-    image: project1,
-  },
-  {
     id: "4",
-    name: "MLK Parade nevada desert experience",
+    name: "MLK peaceWalk nevada desert experience",
     description:
       "Every January in Las Vegas is the second Largest Parade in Las Vegas. The Martin Luther King  Jr Parade is a time for the community to take the streets and celebrate a life that was dedicated to equality and justice. I have had the privilege to organize a community float in the parade over the past 5 years and it is always a great place to meet new people and hear a good message. ",
     link: "http://nevadadesertexperience.org/mlk",
-    image: project1,
+    image: project4,
   },
   {
     id: "5",
@@ -41,7 +45,7 @@ export const DEFAULT_PROJECTS = [
     description:
       "The Nevada Desert has a lot of history and much of its vast treasure is far from the large cities and bright lights. The Nevada Desert Experience has organized a sacred walk into the desert to honor the land and the people who have lived on it for centuries. During my time coordinating this event I have met various individuals from different backgrounds and faiths on this 60 mile pilgrimage. I recommend doing the Sacred Peace Walk once in your life. ",
     link: "http://nevadadesertexperience.org/spw",
-    image: project1,
+    image: project5,
   },
   {
     id: "6",
@@ -49,13 +53,13 @@ export const DEFAULT_PROJECTS = [
     description:
       "Drawing out or designing plans is how I begin most of my projects. It helps make the idea a little more of a reality and it allows me the opportunity to see into the project with greater depth. ",
     link: "http://nevadadesertexperience.org/spw",
-    image: project1,
+    image: project6,
   },
   {
     id: "7",
     name: "Art",
     description: "Personal music",
     link: "https://soundcloud.com/robertmajors",
-    image: project1,
+    image: project7,
   },
 ]
