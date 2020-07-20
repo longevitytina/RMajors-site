3c40917177c3431eb33385068a8256b5f1fc18b9
Tina <longevity.tina@gmail.com>
2020-07-20 15:48:00 -0700

    Import bootstrap css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 import Title from "../components/title"
 import Projects from "../components/projects"
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import About from "../components/about"
 import Testimonials from "../components/testimonials"
+import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
