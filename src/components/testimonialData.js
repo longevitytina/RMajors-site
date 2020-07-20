471b7f900aa04f17ac33f945a37cc925f932cfd1
Tina <longevity.tina@gmail.com>
2020-07-20 16:38:30 -0700

    Styles testimonial section, removes pictures

--- src/components/testimonialData.js
+++ src/components/testimonialData.js
@@ -1,7 +1,3 @@
-import testimonal1 from "../images/testimonal1.jpg"
-import testimonial2 from "../images/testimonial2.jpg"
-import testimonial3 from "../images/testimonials3.jpg"
-
 export const DEFAULT_TESTIMONIALS = [
   {
     id: "1",
@@ -9,7 +5,6 @@ export const DEFAULT_TESTIMONIALS = [
     description:
       "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
     link: "#",
-    image: testimonal1,
   },
   {
     id: "2",
@@ -17,7 +12,6 @@ export const DEFAULT_TESTIMONIALS = [
     description:
       "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
     link: "#",
-    image: testimonial2,
   },
   {
     id: "3",
@@ -25,6 +19,5 @@ export const DEFAULT_TESTIMONIALS = [
     description:
       "Utapau chewbacca mara skywalker sidious tatooine darth. Wampa padmé padmé sidious. Darth calamari kessel skywalker droid alderaan. ",
     link: "#",
-    image: testimonial3,
   },
 ]
--- src/components/testimonialPreview.js
+++ src/components/testimonialPreview.js
@@ -11,12 +11,8 @@ export default function projectPreview({
 }) {
   return (
     <figure>
-      <img src={image} alt={name} className={itemStyles.image} />
       <div className={itemStyles.title}>{name}</div>
       <div className={itemStyles.description}>{description}</div>
-      <a href={link} className={itemStyles.link}>
-        View more
-      </a>
     </figure>
   )
 }
