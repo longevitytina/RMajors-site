77854b5aa8ba7842ae85188dd268f573c060955a
Tina <longevity.tina@gmail.com>
2020-07-20 15:46:10 -0700

    Changes project preview display to Masonary, using bootstrap

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,18 +2,19 @@ import React from "react"
 import ProjectPreview from "./projectPreview"
 import { DEFAULT_PROJECTS } from "./projectData"
 import projectStyles from "./projects.module.css"
+import { CardColumns } from "react-bootstrap"
 
 export default function projects() {
   return (
-    <div>
+    <div className={projectStyles.container}>
       <h1 className={projectStyles.heading} id="projects">
         Projects
       </h1>
-      <div className={projectStyles.container}>
+      <CardColumns>
         {DEFAULT_PROJECTS.map((project, i) => (
           <ProjectPreview {...project} key={i} />
         ))}
-      </div>
+      </CardColumns>
     </div>
   )
 }
