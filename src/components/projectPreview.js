38134337761689f2bc4ee25ba62ec6c2641df799
Tina <longevity.tina@gmail.com>
2020-07-20 15:45:30 -0700

    Adds react-bootstrap card, image only(for now)

--- src/components/projectPreview.js
+++ src/components/projectPreview.js
@@ -1,7 +1,8 @@
 import React from "react"
 import itemStyles from "./projectPreview.module.css"
+import { Card } from "react-bootstrap"
 
-export default function TestimonialPreview({
+export default function projectPreview({
   data,
   name,
   description,
@@ -9,13 +10,18 @@ export default function TestimonialPreview({
   image,
 }) {
   return (
-    <figure>
-      <img src={image} alt={name} className={itemStyles.image} />
-      <div className={itemStyles.title}>{name}</div>
-      <div className={itemStyles.description}>{description}</div>
-      <a href={link} className={itemStyles.link}>
-        View more
-      </a>
-    </figure>
+    <Card>
+      <Card.Img src={image} />
+      {/* // <img src={image} alt={name} className={itemStyles.image} /> */}
+      {/* <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+      </Card.Body> */}
+      {/* <div className={itemStyles.title}>{name}</div> */}
+      {/* <div className={itemStyles.description}>{description}</div> */}
+      {/* <a href={link} className={itemStyles.link}> */}
+      {/* View more */}
+      {/* </a> */}
+    </Card>
   )
 }
