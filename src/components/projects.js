957572b737378cab85445f93374c95a86a2ee2e0
Tina <longevity.tina@gmail.com>
2020-09-25 16:44:56 -0700

    Adds accordion toggle for project section

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,19 +2,31 @@ import React from "react"
 import ProjectPreview from "./projectPreview"
 import { DEFAULT_PROJECTS } from "./projectData"
 import projectStyles from "./projects.module.css"
-import { CardColumns } from "react-bootstrap"
+import { CardColumns, Accordion, Button } from "react-bootstrap"
 
 export default function projects() {
   return (
     <div className={projectStyles.container}>
-      <h1 className={projectStyles.heading} id="projects">
-        Projects
-      </h1>
-      <CardColumns className="pl-5 pr-5">
-        {DEFAULT_PROJECTS.map((project, i) => (
-          <ProjectPreview {...project} key={i} />
-        ))}
-      </CardColumns>
+      <Accordion>
+        <span className={projectStyles.heading} id="projects">
+          Recent Work |
+        </span>
+        <Accordion.Toggle
+          as={Button}
+          className={projectStyles.accordion}
+          variant="link"
+          eventKey="0"
+        >
+          Click me!
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <CardColumns className="pl-5 pr-5">
+            {DEFAULT_PROJECTS.map((project, i) => (
+              <ProjectPreview {...project} key={i} />
+            ))}
+          </CardColumns>
+        </Accordion.Collapse>
+      </Accordion>
     </div>
   )
 }
